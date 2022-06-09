c7e230b03d7c1995d40a9574c19d5da5fbeec22e
mahmod23csis <mahmodahmad23@outlook.com>
2022-06-09 10:26:16 -0500

    comments added by Mahmod

--- csharpxml4/Program.cs
+++ csharpxml4/Program.cs
@@ -9,26 +9,35 @@ namespace ParsingXml
     {
         static void Main(string[] args)
         {
+            // Load XML file to memory
             XDocument xmlDoc = XDocument.Load(@"C:\Users\v-mahahmad.NORTHAMERICA\source\repos\csharpxml4\AddSolutionComponentRequest.xml");
 
+            // Iterable object for Member element children to get all MemberName attributes
             IEnumerable<XElement> xElements = xmlDoc.Descendants("Member");
 
             Console.WriteLine("Validating XML documents for missing description\n");
 
+            // For loop to iterate through each MemberName attribute
             foreach (XElement element in xElements)
             {
+                // Output Member Name to console
                 Console.WriteLine("Member Name: " + element.Attribute("MemberName").Value);
 
+                // summary variable to store elements under summary
                 var summary = element.Descendants("summary");
 
+                // For loop to iterate through each element in summary 
                 foreach (var item in summary)
                 {
+                    // If description says For internal use only, do nothing
                     if (item.Value == "For internal use only")
                         Console.WriteLine("Processed successfully.");
 
+                    // If description is not empty, output a message
                     else if (!String.IsNullOrEmpty(item.Value))
                         Console.WriteLine("This class has summary. Processed successfully.");
 
+                    // Otherwise, if description is empty, print out a message
                     else
                         Console.WriteLine("This class is missing description.");
                 }
