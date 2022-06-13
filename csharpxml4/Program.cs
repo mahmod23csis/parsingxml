aa9db647b8b78444878db8c2b4cb6304f23f2c9e
mahmod23csis <mahmodahmad23@outlook.com>
2022-06-13 16:15:53 -0500

    few changes made 6/13

--- csharpxml4/Program.cs
+++ csharpxml4/Program.cs
@@ -4,50 +4,31 @@ using System.Xml.Linq;
 using System.Text;
 
 
-
 namespace ParsingXml
 {
     class Program
     {
+       // private static XDocument xmlDoc;
         static void Main(string[] args)
         {
-            // Load XML file to memory
-            XDocument xmlDoc = XDocument.Load(@"C:\Users\v-mahahmad.NORTHAMERICA\source\repos\csharpxml4\AddSolutionComponentRequest.xml");
-            
-            // Iterable object for Member element children to get all MemberName attributes
-            IEnumerable<XElement> xElements = xmlDoc.Descendants("Member");
+            XmlFile f = new XmlFile();
 
-            Console.WriteLine("Validating XML documents for missing description\n");
+            Console.Write("Please type your XML Path/URL: ");
+            string MyFile = Console.ReadLine();
 
-            // Output the type name to console
-            Console.WriteLine(xmlDoc.Root.Attribute("Name").Value.ToString());
+            // "C:\Users\v-mahahmad.NORTHAMERICA\source\repos\csharpxml4\AddSolutionComponentRequest.xml"
 
-            // For loop to iterate through each MemberName attribute
-            foreach (XElement element in xElements)
-            {
-                // Output MemberName values to console
-                Console.WriteLine("Member Name: " + element.Attribute("MemberName").Value);
+            // Handle file error
+            // If file doesn't exist or wrong format, prompt again
+            f.ParseFile(MyFile);
 
-                // summary variable to store elements under summary
-                var summary = element.Descendants("summary");
-
-                // For loop to iterate through each element in summary 
-                foreach (var item in summary)
-                {
-                    // If description says For internal use only, do nothing
-                    if (item.Value == "For internal use only")
-                        Console.WriteLine("Processed successfully.\n");
-
-                    // If description is not empty, output a message
-                    else if (!String.IsNullOrEmpty(item.Value))
-                        Console.WriteLine("This class has summary. Processed successfully.\n");
-
-                    // Otherwise, if description is empty, print out a message
-                    else
-                        Console.WriteLine("This class is missing description.\n");
-                }
-            }
             Console.ReadKey();
+            }
+            
+            // Load XML file to memory
+            //XDocument xmlDoc = XDocument.Load(@"C:\Users\v-mahahmad.NORTHAMERICA\source\repos\csharpxml4\AddSolutionComponentRequest.xml");
+            //OpenFile();
+            //XDocument xmlDoc = XDocument.Load(@"C:\Users\v-mahahmad.NORTHAMERICA\source\repos\csharpxml4\AddSolutionComponentRequest.xml");
+            // Iterable object for Member element children to get all MemberName attributes
         }
     }
-}
